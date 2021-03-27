242b99b682bc2e61c80a8352b94bf5ccbebdb058
GalinaSherbak84 <galinasherbak@gmail.com>
2021-03-27 16:16:21 +0100

    rendere gli ingredienti cliccabili

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 //referenze
 var btn = document.getElementById('button');
 var burgerName = document.getElementById('name');
-var ingredients = document.getElementsByClassName('ingredient-checkbox')
+var ingredients = document.getElementsByClassName('ingredient-checkbox');
 //console.log(ingredients);
 var displayPrice = document.getElementById('price');
+var coupon = document.getElementById('coupon');
+var addBtn = document.getElementsByClassName('ingredient-add');
+
+//settings
+var coupons = ['12354ABCDEF', '6789GHKIO', '67jk90lm32d'];
+
+//4 rendere gli ingredienti cliccabili
+for(var i = 0; i < addBtn.length; i++){
+    var add = addBtn[i];
+    add.addEventListener('click', function(){
+        console.log(this);//elemento che ho cliccato.
+        var thisCheckbox = this.previousElementSibling;
+        thisCheckbox.checked = !thisCheckbox.checked;
+    });
+}
 
 //calc prezzo
-btn.addEventListener('click',function(){
+btn.addEventListener('click', function(){
     var name = burgerName.value.trim();
     //console.log(name);
 
@@ -30,6 +45,13 @@ btn.addEventListener('click',function(){
             }
         }
         console.log(price);
+        //3 Sconto
+        var couponCode = coupon.value;
+        //console.log(couponCode);
+        if(coupons.includes(couponCode)){
+            //20%  di sconto
+            price -= price * 0.2
+        }
 
         //2 stampa il prezzo a schermo.
         displayPrice.innerHTML = price.toFixed(2);
